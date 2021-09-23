415a603f10533b5a1a9dc9f9f027395db2dd09d8
DayDream-21 <vova13158@gmail.com>
2021-09-23 23:28:18 +0300

    Created post method addNewEmployee. Now we can add new employee writing json in body of http request.

--- src/main/java/com/slavamashkov/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/slavamashkov/spring/rest/controller/MyRESTController.java
@@ -40,4 +40,11 @@ public class MyRESTController {
 
         return employee;
     }
+
+    @PostMapping("/employees")
+    public Employee addNewEmployee(@RequestBody Employee employee) {
+        employeeService.saveEmployee(employee);
+
+        return employee;
+    }
 }
